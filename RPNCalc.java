f4d85c96d0492b3c54cb1a94b83ec5114673660b
Gark <g.knott90@gmail.com>
2013-10-07 16:36:31 -0400

    Update V2

--- RPNCalc.java
+++ RPNCalc.java
@@ -21,10 +21,12 @@ import java.util.StringTokenizer;
 public class RPNCalc{
 	public static void main(String[] args) throws IOException{
 	
+	//Initialize the IO objects and Stack objects that are used in the program.
 	InputStreamReader stream = new InputStreamReader(System.in);
 	BufferedReader bf = new BufferedReader(stream);
 	Stack<Double> rpnStack = new Stack<Double>();
 	
+	//Initializing value which will hold the token.  And first and second which performs the arithmetic below.
 	Double value, first, second;
 	String token;
 	
@@ -32,14 +34,20 @@ public class RPNCalc{
 	System.out.println("Enter 'quit' in order to terminate the program.");
 	System.out.println();
 	
+	//infinite loop
 	while(true)
 	{
+	//Read in calculation, check if user ask to terminate program
 	System.out.print(">>");
 	String line = bf.readLine();
 	if(line.equals("quit"))
 	{
 		System.exit(0);
 	}
+	
+	//Uses StringTokenizer to go through the entered line.  Uses an empty space as a deliminator.
+	//If the value is a Double, it is pushed on to the stack
+	//If the value is a operator, perform the arithmetic seen in the switch statement.
 	StringTokenizer stok = new StringTokenizer(line," ",false);
 	while(stok.hasMoreElements()){
 	    token = stok.nextToken();
@@ -48,7 +56,9 @@ public class RPNCalc{
 		   second = ((Double)rpnStack.pop()).doubleValue();
 		   first  = ((Double)rpnStack.pop()).doubleValue();
 		   char character = token.charAt(0);
-		   switch(character){
+		   
+		   switch(character)
+		   {
 		   case '+': rpnStack.push(second + first); break;
 		   case '-': rpnStack.push(first - second); break;
 		   case '/': rpnStack.push(first/second);   break;
